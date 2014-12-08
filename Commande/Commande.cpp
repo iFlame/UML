68b7930ab645821788c24e17f3c1a369fb68f187
Clement Audry <audry@polytech.unice.frgit add http///audry@atlassian.polytech.unice.fr:7990/scm/OONPH/public.git NP4Hgit config --global user.email audry@polytech.unice.fr>
2014-12-08 09:28:06 +0100

    testouille

--- Commande/Commande.cpp
+++ Commande/Commande.cpp
@@ -1,8 +1,17 @@
 #include "Commande.h"
+#include <algorithm>
 
 Commande::Commande()
 {
    // commande["AVANCER"];
 }
 void Commande::execute(){}
-Commande* Commande::creerCommande(string){return NULL;}
+
+Commande* Commande::creerCommande(string comm){
+    transform(comm.begin(), comm.end(),comm.begin(), ::toupper);
+    istringstream iss(comm, istringstream::in);
+    iss>>arg;
+    iss>>arg2;
+    iss>>arg3;
+    return commande[arg];
+}
--- Commande/Commande.h
+++ Commande/Commande.h
@@ -1,6 +1,7 @@
 #ifndef COMMANDE_H
 #define COMMANDE_H
 #include <map>
+
 using namespace std;
 
 
@@ -8,8 +9,12 @@ using namespace std;
 class Commande
 {
     public:
+        string arg;
+        string arg2:
+        string arg3;
+
         Commande();
-        map<string,Commande> commande;
+        static map<string,Commande*>& commande();
         virtual void execute();
         Commande* creerCommande(string);
     protected:
